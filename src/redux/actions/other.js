fd34aa52897d5997fd023d764277df82c4484b63
kartik0027 <147530370+kartik0027@users.noreply.github.com>
2023-10-11 21:14:42 +0530

    Update other.js
    path changes

--- src/redux/actions/other.js
+++ src/redux/actions/other.js
@@ -5,7 +5,7 @@ export const contactUs = (name, email, message) => async dispatch => {
   try {
     dispatch({ type: 'contactRequest' });
     const { data } = await axios.post(
-      `${server}/contact`,
+      `${server}/other/contact`,
       { name, email, message },
       {
         headers: {
@@ -25,7 +25,7 @@ export const courseRequest = (name, email, course) => async dispatch => {
   try {
     dispatch({ type: 'courseRequestRequest' });
     const { data } = await axios.post(
-      `${server}/courserequest`,
+      `${server}/course/courserequest`,
       { name, email, course },
       {
         headers: {
